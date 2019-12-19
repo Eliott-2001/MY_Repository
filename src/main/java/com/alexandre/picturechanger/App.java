fbc9fbef6d61c7158cd4f449e34fa90c9724bb42
JulienViseux <julienviseux95@gmail.com>
2019-12-19 14:55:26 +0100

    Logger

--- src/main/java/com/alexandre/picturechanger/App.java
+++ src/main/java/com/alexandre/picturechanger/App.java
@@ -1,20 +1,52 @@
 package com.alexandre.picturechanger;
 
         import com.alexandre.picturechanger.filters.FilterBlur;
+
+        import com.alexandre.picturechanger.filters.FilterGrayscale;
+        import org.apache.commons.cli.*;
         import org.bytedeco.opencv.global.opencv_imgcodecs;
         import org.bytedeco.opencv.opencv_core.Mat;
 
         import java.io.File;
         import java.io.IOException;
+        import java.util.Arrays;
 
 public class App {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
 
+        Options options= new Options();
+        options.addOption("i","input-dir",true,"Input directory");
+        options.addOption("o","output-dir",true,"Output directory");
+        //options.addOption("f","filter",true,"Name of the filter");
+        //options.addOption("h","help",false,"This is help");
 
-        FilterBlur fb = new FilterBlur();
+        String inputDirArg = "imgs";
+        String outputDirArg = "output";
 
-            File inputDir = new File("imgs");
+        CommandLineParser parser = new DefaultParser();
+        try{
+            CommandLine cmd = parser.parse(options,args);
+            if(cmd.hasOption("i")){
+                inputDirArg=cmd.getOptionValue("i");
+                System.out.println(inputDirArg);
+            }
+            if(cmd.hasOption("o")){
+                outputDirArg=cmd.getOptionValue("o");
+                System.out.println(outputDirArg);
+            }
+            //if(cmd.hasOption("f")){
+            //    filterArg = cmd.getOptionValue("f");
+            //    System.out.println(filterArg);
+            //}
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+      FilterGrayscale fg = new FilterGrayscale();
+
+            File inputDir = new File(inputDirArg);
             File[] files = inputDir.listFiles();
 
 
@@ -23,12 +55,12 @@ public class App {
                 Mat image = opencv_imgcodecs.imread(f.getAbsolutePath());
 
                 try{
-                    image = fb.filterBlur(image, f.getName());
+                    image = fg.filterGrayscale(image, f.getName());
                 }catch (FiltrerExecption | IOException e){
                   System.out.println(e.getMessage());
                 }
 
-                File outputDir = new File("output");
+                File outputDir = new File(outputDirArg);
                 File outputFile = new File(outputDir, f.getName());
                 opencv_imgcodecs.imwrite(outputFile.getAbsolutePath(), image);
             }
--- src/main/java/com/alexandre/picturechanger/filters/FilterGrayscale.java
+++ src/main/java/com/alexandre/picturechanger/filters/FilterGrayscale.java
@@ -1,5 +1,6 @@
 package com.alexandre.picturechanger.filters;
 
+import com.alexandre.picturechanger.FiltrerExecption;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.opencv.core.CvType;
 import org.opencv.imgproc.Imgproc;
@@ -11,7 +12,7 @@ import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
 
 public class FilterGrayscale {
 
-    public Mat filterGrayscale(Mat image, String name) throws IOException {
+    public Mat filterGrayscale(Mat image, String name) throws FiltrerExecption, IOException {
         Mat result = new Mat(image.rows(), image.cols(), CvType.CV_8UC3);
         cvtColor(image, result, Imgproc.COLOR_RGB2GRAY);
         FileWriter fw = new FileWriter("imagefilter.log", true);
